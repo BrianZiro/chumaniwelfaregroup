edfc7995902ddd400177c5c933146564b20f38fb
BrianZiro <brianziro44@gmail.com>
2025-09-17 17:06:30 +0300

    tackled bad request

--- welfare_website/settings.py
+++ welfare_website/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-84r*0#22!o^2m!le6w9f=a$eb0^)z#d)k7^e!2&9hf99+5y@fc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['chumaniwelfare.com']
+ALLOWED_HOSTS = ['chumaniwelfare.com', 'www.chumaniwelfare.com']
 
 
 MEDIA_URL = '/media/'
