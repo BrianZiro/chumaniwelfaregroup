111077e5f0b4ed85a93d0fd9b3147ea68b7b6aae
BrianZiro <brianziro44@gmail.com>
2025-09-17 13:24:26 +0300

    deployment debugging

--- welfare_website/settings.py
+++ welfare_website/settings.py
@@ -117,13 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# URL for static files
+STATIC_URL = '/static/'
 
+# Where collectstatic will gather all static files for deployment
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Additional folders where Django will look for static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
