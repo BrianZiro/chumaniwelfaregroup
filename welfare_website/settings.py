55d671da9e12049c9bb904ec0ce7b101fe68dbe8
BrianZiro <brianziro44@gmail.com>
2025-09-17 17:25:18 +0300

    host debugging

--- welfare_website/settings.py
+++ welfare_website/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-84r*0#22!o^2m!le6w9f=a$eb0^)z#d)k7^e!2&9hf99+5y@fc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+DEBUG = os.getenv("DEBUG") == "True"
 
 
 MEDIA_URL = '/media/'
