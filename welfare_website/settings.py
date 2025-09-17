520271610a1b09df12eb43418be386d065839b45
BrianZiro <brianziro44@gmail.com>
2025-09-17 17:30:35 +0300

    debug_true

--- welfare_website/settings.py
+++ welfare_website/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = 'django-insecure-84r*0#22!o^2m!le6w9f=a$eb0^)z#d)k7^e!2&9hf99+5y@fc
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = True
+ALLOWED_HOSTS = ['*']
 
 
 MEDIA_URL = '/media/'
